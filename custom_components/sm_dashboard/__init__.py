063d33af6f84e8bc46a93555800b2f3c83a4ed0c
Gertjan <gertjan.stulp@friss.com>
2022-02-13 23:16:44 +0100

    update

--- custom_components/sm_dashboard/__init__.py
+++ custom_components/sm_dashboard/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-from .process_yaml import process_yaml
+from .process_yaml import bootstrap
 # from .notifications import notifications
 
 _LOGGER = logging.getLogger(__name__)
@@ -8,32 +8,21 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = "sm_dashboard"
 
 async def async_setup(hass, config):
-    # hass.data[DOMAIN] = {
-    #     "notifications": {},
-    #     "commands": {},
-    #     'latest_version': ""
-    # }
+    hass.data[DOMAIN] = {
+        "notifications": {},
+        "commands": {},
+        'latest_version': ""
+    }
 
     # notifications(hass, DOMAIN)
     
+    bootstrap()
+
     return True
 
 async def async_setup_entry(hass, entry):
-    process_yaml(hass, entry)
-
-    # config_entry.add_update_listener(_update_listener) 
-
     return True
 
 
 async def async_remove_entry(hass, entry):
     return True
-
-# async def _update_listener(hass, config_entry):
-#     _LOGGER.warning('Update_listener called')
-
-#     process_yaml(hass, config_entry)
-
-#     hass.bus.async_fire("sm_dashboard_reload")
-
-#     return True
--- custom_components/sm_dashboard/process_yaml.py
+++ custom_components/sm_dashboard/process_yaml.py
@@ -1,6 +1,9 @@
 import logging
 import os
 import logging
+import json
+import io
+import jinja2
 from collections import OrderedDict
 
 from homeassistant.util.yaml import loader
@@ -8,32 +11,50 @@ from homeassistant.exceptions import HomeAssistantError
 
 _LOGGER = logging.getLogger(__name__)
 
-# def load_yamll(fname, secrets = None, args={}):
-#     try:
-#         _LOGGER.warn('sm dashboard')
-#         with open(fname, encoding="utf-8") as config_file:
-#             return loader.yaml.load(config_file, Loader=lambda stream: loader.SafeLineLoader(stream, secrets)) or OrderedDict()
-#     except loader.yaml.YAMLError as exc:
-#         _LOGGER.error(str(exc))
-#         raise HomeAssistantError(exc)
-#     except UnicodeDecodeError as exc:
-#         _LOGGER.error("Unable to read file %s: %s", fname, exc)
-#         raise HomeAssistantError(exc)
+def fromjson(value):
+    return json.loads(value)
 
+jinja = jinja2.Environment(loader=jinja2.FileSystemLoader("/"))
 
-# def _include_yaml(ldr, node):
-#     args = {}
-#     if isinstance(node.value, str):
-#         fn = node.value
-#     else:
-#         fn, args, *_ = ldr.construct_sequence(node)
-#     fname = os.path.abspath(os.path.join(os.path.dirname(ldr.name), fn))
-#     try:
-#         return loader._add_reference(load_yamll(fname, ldr.secrets, args=args), ldr, node)
-#     except FileNotFoundError as exc:
-#         _LOGGER.error("Unable to include file %s: %s", fname, exc);
-#         raise HomeAssistantError(exc)
+jinja.filters['fromjson'] = fromjson
 
-_LOGGER.error("test sm_dashboard")
-# loader.load_yaml = load_yamll
-# loader.SafeLineLoader.add_constructor("!include", _include_yaml)
+def load_yamll(fname, secrets = None, args={}):
+    try:
+        process_yaml = False
+        with open(fname, encoding="utf-8") as f:
+            if f.readline().lower().startswith(("# sm_dashboard")):
+                process_yaml = True
+
+        if process_yaml:
+            stream = io.StringIO(jinja.get_template(fname).render({
+                **args
+                }))
+            stream.name = fname
+            return loader.yaml.load(stream, Loader=lambda _stream: loader.SafeLineLoader(_stream, secrets)) or OrderedDict()
+        else:
+            with open(fname, encoding="utf-8") as config_file:
+                return loader.yaml.load(config_file, Loader=lambda stream: loader.SafeLineLoader(stream, secrets)) or OrderedDict()
+    except loader.yaml.YAMLError as exc:
+        _LOGGER.error(str(exc))
+        raise HomeAssistantError(exc)
+    except UnicodeDecodeError as exc:
+        _LOGGER.error("Unable to read file %s: %s", fname, exc)
+        raise HomeAssistantError(exc)
+
+def _include_yaml(ldr, node):
+    args = {}
+    if isinstance(node.value, str):
+        fn = node.value
+    else:
+        fn, args, *_ = ldr.construct_sequence(node)
+    fname = os.path.abspath(os.path.join(os.path.dirname(ldr.name), fn))
+    try:
+        return loader._add_reference(load_yamll(fname, ldr.secrets, args=args), ldr, node)
+    except FileNotFoundError as exc:
+        _LOGGER.error("Unable to include file %s: %s", fname, exc);
+        raise HomeAssistantError(exc)
+
+def bootstrap():
+    loader.load_yaml = load_yamll
+    loader.SafeLineLoader.add_constructor("!include", _include_yaml)
+    
