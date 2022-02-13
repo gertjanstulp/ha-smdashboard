a4779545d0df8065111c03419e1dde688ff5734e
Gertjan <gertjan.stulp@friss.com>
2022-02-13 22:25:22 +0100

    update

--- custom_components/sm_dashboard/__init__.py
+++ custom_components/sm_dashboard/__init__.py
@@ -8,20 +8,20 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = "sm_dashboard"
 
 async def async_setup(hass, config):
-    hass.data[DOMAIN] = {
-        "notifications": {},
-        "commands": {},
-        'latest_version': ""
-    }
+    # hass.data[DOMAIN] = {
+    #     "notifications": {},
+    #     "commands": {},
+    #     'latest_version': ""
+    # }
 
     # notifications(hass, DOMAIN)
     
     return True
 
-async def async_setup_entry(hass, config_entry):
-    process_yaml(hass, config_entry)
+async def async_setup_entry(hass, entry):
+    process_yaml(hass, entry)
 
-    config_entry.add_update_listener(_update_listener) 
+    # config_entry.add_update_listener(_update_listener) 
 
     return True
 
@@ -29,11 +29,11 @@ async def async_setup_entry(hass, config_entry):
 async def async_remove_entry(hass, entry):
     return True
 
-async def _update_listener(hass, config_entry):
-    _LOGGER.warning('Update_listener called')
+# async def _update_listener(hass, config_entry):
+#     _LOGGER.warning('Update_listener called')
 
-    process_yaml(hass, config_entry)
+#     process_yaml(hass, config_entry)
 
-    hass.bus.async_fire("sm_dashboard_reload")
+#     hass.bus.async_fire("sm_dashboard_reload")
 
-    return True
+#     return True
--- custom_components/sm_dashboard/process_yaml.py
+++ custom_components/sm_dashboard/process_yaml.py
@@ -8,32 +8,32 @@ from homeassistant.exceptions import HomeAssistantError
 
 _LOGGER = logging.getLogger(__name__)
 
-def load_yamll(fname, secrets = None, args={}):
-    try:
-        _LOGGER.warn('sm dashboard')
-        with open(fname, encoding="utf-8") as config_file:
-            return loader.yaml.load(config_file, Loader=lambda stream: loader.SafeLineLoader(stream, secrets)) or OrderedDict()
-    except loader.yaml.YAMLError as exc:
-        _LOGGER.error(str(exc))
-        raise HomeAssistantError(exc)
-    except UnicodeDecodeError as exc:
-        _LOGGER.error("Unable to read file %s: %s", fname, exc)
-        raise HomeAssistantError(exc)
+# def load_yamll(fname, secrets = None, args={}):
+#     try:
+#         _LOGGER.warn('sm dashboard')
+#         with open(fname, encoding="utf-8") as config_file:
+#             return loader.yaml.load(config_file, Loader=lambda stream: loader.SafeLineLoader(stream, secrets)) or OrderedDict()
+#     except loader.yaml.YAMLError as exc:
+#         _LOGGER.error(str(exc))
+#         raise HomeAssistantError(exc)
+#     except UnicodeDecodeError as exc:
+#         _LOGGER.error("Unable to read file %s: %s", fname, exc)
+#         raise HomeAssistantError(exc)
 
 
-def _include_yaml(ldr, node):
-    args = {}
-    if isinstance(node.value, str):
-        fn = node.value
-    else:
-        fn, args, *_ = ldr.construct_sequence(node)
-    fname = os.path.abspath(os.path.join(os.path.dirname(ldr.name), fn))
-    try:
-        return loader._add_reference(load_yamll(fname, ldr.secrets, args=args), ldr, node)
-    except FileNotFoundError as exc:
-        _LOGGER.error("Unable to include file %s: %s", fname, exc);
-        raise HomeAssistantError(exc)
+# def _include_yaml(ldr, node):
+#     args = {}
+#     if isinstance(node.value, str):
+#         fn = node.value
+#     else:
+#         fn, args, *_ = ldr.construct_sequence(node)
+#     fname = os.path.abspath(os.path.join(os.path.dirname(ldr.name), fn))
+#     try:
+#         return loader._add_reference(load_yamll(fname, ldr.secrets, args=args), ldr, node)
+#     except FileNotFoundError as exc:
+#         _LOGGER.error("Unable to include file %s: %s", fname, exc);
+#         raise HomeAssistantError(exc)
 
-_LOGGER.warning("test sm_dashboard")
-loader.load_yaml = load_yamll
-loader.SafeLineLoader.add_constructor("!include", _include_yaml)
+_LOGGER.error("test sm_dashboard")
+# loader.load_yaml = load_yamll
+# loader.SafeLineLoader.add_constructor("!include", _include_yaml)
